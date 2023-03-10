7249dfaaadcdd72a27a77054ef74fa31f330ca5d
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-10 07:07:21 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,34 @@ import os
 api_id = 9910861
 api_hash = "86e927460a8998ba6d84e9c13acfda95"
 bot_token = os.environ.get('bot_token')
-Channel_Id = -1001804018431
+Channel_Id = 'FreeXDownloader'
 bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
 
+#Acceso
+async def participant(user_id: int):
+    if Channel_Id is None:
+        return True
+    try:
+        await bot.get_chat_member(Channel_Id, user_id)
+    except ChatAdminRequired:
+        print(f"Please Add the Bot to @{Config.Bot_Channel} as Admin")
+        return True
+    except UserNotParticipant:
+        buttons = [[InlineKeyboardButton('Unete Para Usar El BoT', url=f'https://t.me/{Channel_Id}')]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await bot.send_message(user_id, 'Unete', reply_markup=reply_markup)
+        return False
+    else:
+        return True
+#Comandos
+@bot.on_message(filters.command('start' & filters.private & filters.incoming)
+async def start(bot, message):
+    await wait(message.chat.id)
+    username = message.from_user.username
+
+    if not await participant(msg.chat.id):
+        return
+    await bot.send_message(username, 'Hola')
 
 
 

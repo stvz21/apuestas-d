4b0344bda6f4e2864cb900aca1494bfbf97833a3
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-10 07:11:04 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 from pyrogram import Client , filters
 from pyrogram.types import Message, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 import os
-
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 api_id = 9910861
 api_hash = "86e927460a8998ba6d84e9c13acfda95"
 bot_token = os.environ.get('bot_token')
-Channel_Id = 'FreeXDownloader'
+Channel_Id = 'FreeZoneDownloader'
 bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
 
 #Acceso

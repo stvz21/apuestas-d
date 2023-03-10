14b57b65ff7ae6f99200b67b154146b55b97c51d
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-10 06:50:04 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+from pyrogram import Client , filters
+from pyrogram.types import Message, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
+api_id = 9910861
+api_hash = "86e927460a8998ba6d84e9c13acfda95"
+bot_token = os.environ.get('bot_token')
+Channel_Id = -1001804018431
+bot = Client("bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
+
+
+
+
+bot.start()
+bot.send_message(5416296262,'**BoT Iniciado**')
+bot.loop.run_forever()

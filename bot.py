d0beaaae44e2350407fc93cedc794d42349cc15a
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-15 07:03:23 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ async def start(bot, message):
     username = message.from_user.username  
    # USERS[username] = {'saldo': 0}
     await bot.send_message(5416296262, 'El Usuario @'+username+' Inicio el BoT')
- #   await bot.send_message(5416296262, USERS)
+    await bot.send_message(5416296262, USERS)
     await send('Hola, Bienvenido al bot de apuestas 🎰')
  #   await bot.edit_message_text(-1001807229422,message_id=5,text="Hola, Viste edite el mensaje")
     msg = await bot.get_messages(-1001807229422,message_ids=5)
@@ -27,14 +27,32 @@ async def start(bot, message):
   #  await send(msg.text)
  #   await bot.send_message(5416296262, USERS)
    # await bot.edit_message_text(-1001807229422,message_id=5,text="Hola, Viste edite el mensaje")
-    USERS = msg.text
-    await bot.send_message(5416296262, USERS)
+    #USERS = msg.text
+   # await bot.send_message(5416296262, USERS)
 
 @bot.on_message(filters.command('help') & filters.private & filters.incoming)
 async def help(bot, message):
     send = message.reply
     username = message.from_user.username
     await bot.send_message(5416296262, USERS)
+
+
+
+def update(username):
+    USERS[username] = {"saldo": 0}
+
+async def get_messages():
+    msg = await bot.get_messages(-1001807229422,message_ids=5)
+    USERS.update(loads(msg.text))
+
+bot.on_message(filters.command('db') & filters.private & filters.incoming)
+async def db(bot, message):
+    send = message.reply
+    username = message.from_user.username
+    await send(Mensaje 1)
+    get_messages()
+
+
 bot.start()
 bot.send_message(5416296262,'**BoT Iniciado**')
 bot.loop.run_forever()

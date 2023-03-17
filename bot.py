7139748e83284043151c85a5e62211798cf1bc33
stvz21 <126835371+stvz21@users.noreply.github.com>
2023-03-17 08:48:22 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ async def callback(bot, msg: CallbackQuery):
         ) 
     elif msg.data == "inicio":
         await msg.mesaage.edit(
-            text=""Hola, Bienvenido al BoT de Apuestas 🎰 Deportivas de Cuba🇨🇺",
+            text="Hola, Bienvenido al BoT de Apuestas 🎰 Deportivas de Cuba🇨🇺",
             reply_markup=inicio
         )
 #Comandos
